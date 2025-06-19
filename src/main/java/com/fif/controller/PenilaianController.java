3edb6dd466607d4c69dcebb369cf078d73573f2a
vincentbharata <vincentbharataa@gmail.com>
2025-06-19 09:44:28 +0700

    update bug

--- src/main/java/com/fif/controller/PenilaianController.java
+++ src/main/java/com/fif/controller/PenilaianController.java
@@ -4,42 +4,22 @@ import java.util.List;
 
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.select.SelectorComposer;
-import org.zkoss.zk.ui.select.annotation.Listen;
 import org.zkoss.zk.ui.select.annotation.Wire;
 import org.zkoss.zul.Button;
-import org.zkoss.zul.Intbox;
 import org.zkoss.zul.ListModelList;
 import org.zkoss.zul.Listbox;
 import org.zkoss.zul.Listcell;
 import org.zkoss.zul.Listitem;
-import org.zkoss.zul.Textbox;
-import org.zkoss.zul.Include;
-
 
 import com.fif.model.Penilaian;
 import com.fif.service.PenilaianService;
 
 public class PenilaianController extends SelectorComposer<Component> {
-    private PenilaianService service = new PenilaianService();
+
+    private final PenilaianService service = new PenilaianService();
 
     @Wire
     private Listbox listboxPenilaian;
-
-    @Wire
-    private Textbox nama, course;
-
-    @Wire
-    private Intbox nilai;
-
-    private Penilaian selected;
-
-    @Wire
-    private Include mainInclude;
-
-    @Listen("onClick = #btnDashboard")
-    public void goToDashboard() {
-        mainInclude.setSrc("/pages/Dashboard.zul");
-    }
 
     @Override
     public void doAfterCompose(Component comp) throws Exception {
@@ -61,10 +41,7 @@ public class PenilaianController extends SelectorComposer<Component> {
             Button btnDelete = new Button("Delete");
 
             btnEdit.addEventListener("onClick", e -> {
-                selected = data;
-                nama.setValue(data.getNama());
-                course.setValue(data.getCourse());
-                nilai.setValue(data.getNilai());
+                org.zkoss.zk.ui.Executions.sendRedirect("index.zul?id=" + data.getId());
             });
 
             btnDelete.addEventListener("onClick", e -> {
@@ -76,27 +53,5 @@ public class PenilaianController extends SelectorComposer<Component> {
             aksi.appendChild(btnDelete);
             item.appendChild(aksi);
         });
-    }
-
-    @Listen("onClick = #btnSimpan")
-    public void onSave() {
-        if (selected == null) {
-            Penilaian p = new Penilaian();
-            p.setNama(nama.getValue());
-            p.setCourse(course.getValue());
-            p.setNilai(nilai.getValue());
-            service.save(p);
-        } else {
-            selected.setNama(nama.getValue());
-            selected.setCourse(course.getValue());
-            selected.setNilai(nilai.getValue());
-            service.update(selected);
-            selected = null;
-        }
-
-        nama.setValue("");
-        course.setValue("");
-        nilai.setValue(null);
-        refreshData();
     }
 }
