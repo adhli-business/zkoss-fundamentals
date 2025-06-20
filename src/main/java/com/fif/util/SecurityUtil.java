d798147dd8868debee3e758007b3d8d8958eddf5
Putra Adhli Falah <71480@fif.co.id>
2025-06-20 17:08:26 +0700

    add SecurityUtil.java

--- src/main/java/com/fif/util/SecurityUtil.java
+++ src/main/java/com/fif/util/SecurityUtil.java
@@ -1,5 +1,14 @@
-// package com.fif.util;
+package com.fif.util;
 
-// public class SecurityUtil {
-    
-// }
+import org.springframework.security.core.Authentication;
+
+public class SecurityUtil {
+    public static boolean hasRole(String role){
+        Authentication authentication = org.springframework.security.core.context.SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated() || authentication.getAuthorities() == null) {
+            return false;
+        }
+        return authentication.getAuthorities().stream()
+                .anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals(role));
+    }
+}
